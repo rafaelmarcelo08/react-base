8ab9c74b2e02d136df25156a93640b6418a6fa59
Rafael <rafael.2018.ads@gmail.com>
2023-03-07 14:31:37 -0300

    MyRoute - Rota personalizada e privada

--- src/pages/Page404/index.js
+++ src/pages/Page404/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import {  } from "./style";
+import { } from "./style";
 import { Container } from "../../styles/GlobalStyles";
 
 export default function Page404() {
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,15 @@
 import React from "react";
-import { Route, Switch } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 
 import Login from "../pages/Login";
 import Page404 from "../pages/Page404";
+import MyRoute from './MyRoute';
 
 export default function Routes() {
   return (
     <Switch>
-      <Route path='/' component={Login} exact />
-      <Route path='*' component={Page404} />
+      <MyRoute path='/' component={Login} exact isClosed />
+      <MyRoute path='*' component={Page404} />
     </Switch>
   );
 }
